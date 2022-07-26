671bf76aa6cf991d09901355352e31128996d942
miljkovich <miljan6888@gmail.com>
2022-07-26 08:29:07 +0200

    Ran Migrations

---
+++ HotelReservationSystem/Migrations/20220726061205_Added new fields to ApplicationUser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelReservationSystem.Migrations
+{
+    public partial class AddednewfieldstoApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                schema: "Identity",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nationality",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Passport",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Nationality",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Passport",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                schema: "Identity",
+                table: "User");
+        }
+    }
+}
