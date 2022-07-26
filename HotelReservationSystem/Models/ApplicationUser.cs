4143f59078ebb2e7c30523dbf9910a0ad5cf7a00
miljkovich <miljan6888@gmail.com>
2022-07-26 08:28:40 +0200

    Added more fields to 'ApplicationUser'

--- HotelReservationSystem/Models/ApplicationUser.cs
+++ HotelReservationSystem/Models/ApplicationUser.cs
@@ -6,6 +6,12 @@ namespace HotelReservationSystem.Models
     {
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+        public String Nationality { get; set; } = null!;
+        public String Passport { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string? Address { get; set; }
+        public string? ZipCode { get; set; }
         public int UsernameChangeLimit { get; set; } = 10;
         public byte[] ?ProfilePicture { get; set; }
         public ICollection<Reservation> ?Reservations { get; set; }
