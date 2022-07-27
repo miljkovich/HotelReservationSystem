648fb26f66c2ee0f91a307ff19ad5491a786f57d
miljkovich <miljan6888@gmail.com>
2022-07-27 23:29:09 +0200

    added Name property to RoomType class

--- HotelReservationSystem/Models/RoomType.cs
+++ HotelReservationSystem/Models/RoomType.cs
@@ -1,12 +1,26 @@
-﻿namespace HotelReservationSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
 {
     public class RoomType
     {
         public int Id { get; set; }
+        [Display(Name = "Имя")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Цена")]
         public int Price { get; set; }
+
+        [Display(Name = "Вместимость")]
         public int Capacity { get; set; }
+
+        [Display(Name = "Описание")]
         public string ?Description { get; set; }
+
+        [Display(Name = "Площадь")]
         public int RoomArea { get; set; }
+
+        [Display(Name = "Фото URL")]
         public string? RoomImageURL { get; set; }
 
         public ICollection<Room>? Rooms { get; set; }
