6409f0b46fc5863fcf646e9ec5d5759731c33ba7
miljkovich <miljan6888@gmail.com>
2022-07-29 14:47:26 +0200

    added display names for Reservation model

--- HotelReservationSystem/Models/Reservation.cs
+++ HotelReservationSystem/Models/Reservation.cs
@@ -1,13 +1,21 @@
-﻿namespace HotelReservationSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
 {
     public class Reservation
     {
         public int Id { get; set; }
+        [Display(Name = "Пользователь Id")]
         public string ApplicationUserId { get; set; } = null!;
+        [Display(Name = "Пользователь")]
         public ApplicationUser ApplicationUser { get; set; } = null!;
+        [Display(Name = "Номер Id")]
         public int RoomId { get; set; }
+        [Display(Name = "Номер")]
         public Room Room { get; set; } = null!;
+        [Display(Name = "Дата заселения")]
         public DateTime DateIn { get; set; }
+        [Display(Name = "Дата выселения")]
         public DateTime DateOut { get; set; }
 
     }
