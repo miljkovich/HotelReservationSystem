899bcffd91004926c740051edf438d4a66555baa
miljkovich <miljan6888@gmail.com>
2022-07-31 16:23:06 +0200

    Added displayNames for ApplicationUser properties

--- HotelReservationSystem/Models/ApplicationUser.cs
+++ HotelReservationSystem/Models/ApplicationUser.cs
@@ -1,20 +1,32 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelReservationSystem.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "Имя")]
         public string FirstName { get; set; } = null!;
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; } = null!;
+        [Display(Name = "Полное Имя")]
         public string FullName { get { return FirstName + " " + LastName; } }
+        [Display(Name = "Дата Рождения")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Гражданство")]
         public String Nationality { get; set; } = null!;
+        [Display(Name = "Пасспорт")]
         public String Passport { get; set; } = null!;
+        [Display(Name = "Город")]
         public string City { get; set; } = null!;
+        [Display(Name = "Адрес")]
         public string? Address { get; set; }
+        [Display(Name = "Почтовой Индекс")]
         public string? ZipCode { get; set; }
         public int UsernameChangeLimit { get; set; } = 10;
+        [Display(Name = "Фото профиля")]
         public byte[] ?ProfilePicture { get; set; }
+        [Display(Name = "Резервации")]
         public ICollection<Reservation> ?Reservations { get; set; }
 
     }
