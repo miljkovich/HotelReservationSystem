83322248078693752f2efc5d178d0c1bfb6e91b2
miljkovich <miljan6888@gmail.com>
2022-07-31 16:24:22 +0200

    ADded UsersController and index view

---
+++ HotelReservationSystem/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using HotelReservationSystem.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservationSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Admin/Users
+
+        public async Task<IActionResult> Index()
+        {
+            return _context.Users != null ?
+                View(await _context.Users.ToListAsync()) :
+                Problem("Entity set 'ApplicationDbContext.Users'  is null.");
+        }
+
+    }
+}
