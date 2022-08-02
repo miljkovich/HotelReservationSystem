5eac69a38e52ae7745ec80bbc2635118a7aa30bd
miljkovich <miljan6888@gmail.com>
2022-08-02 17:26:30 +0200

    ADded field null restrictions to EditUserViewModel

--- HotelReservationSystem/Areas/Admin/Models/EditUserViewModel.cs
+++ HotelReservationSystem/Areas/Admin/Models/EditUserViewModel.cs
@@ -6,15 +6,15 @@
         {
             Roles = new List<string>();
         }
-        public string Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
         public DateTime BirthDate { get; set; }
-        public string Nationality { get; set; }
-        public string Passport { get; set; }
-        public string City { get; set; }
-        public string Address { get; set; }
-        public string ZipCode { get; set; }
+        public string Nationality { get; set; } = null!;
+        public string Passport { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string? Address { get; set; }
+        public string? ZipCode { get; set; }
         public byte[]? ProfilePicture { get; set; }
         public IList<string>? Roles { get; set; }
     }
