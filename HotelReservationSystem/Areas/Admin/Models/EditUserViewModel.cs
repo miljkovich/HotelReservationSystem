7ca380f6cec4d28fe89c26e317ae99180a1a1a73
miljkovich <miljan6888@gmail.com>
2022-08-02 17:12:57 +0200

    Added EditUserVIewModel

---
+++ HotelReservationSystem/Areas/Admin/Models/EditUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace HotelReservationSystem.Areas.Admin.Models
+{
+    public class EditUserViewModel
+    {
+        public EditUserViewModel()
+        {
+            Roles = new List<string>();
+        }
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Nationality { get; set; }
+        public string Passport { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public byte[]? ProfilePicture { get; set; }
+        public IList<string>? Roles { get; set; }
+    }
+}
