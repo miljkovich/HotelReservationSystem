769eeb96210846ce3bd516676d4577da33eef8df
miljkovich <miljan6888@gmail.com>
2022-08-03 14:57:52 +0200

    Changed capacity to AdultsCapacity and KidsCapacity

---
+++ HotelReservationSystem/Migrations/20220803125021_Added adults and kids capacity.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelReservationSystem.Migrations
+{
+    public partial class Addedadultsandkidscapacity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Capacity",
+                schema: "Identity",
+                table: "RoomTypes",
+                newName: "KidsCapacity");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AdultsCapacity",
+                schema: "Identity",
+                table: "RoomTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "hasBath",
+                schema: "Identity",
+                table: "RoomTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "hasPhone",
+                schema: "Identity",
+                table: "RoomTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "hasTV",
+                schema: "Identity",
+                table: "RoomTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "hasWiFi",
+                schema: "Identity",
+                table: "RoomTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdultsCapacity",
+                schema: "Identity",
+                table: "RoomTypes");
+
+            migrationBuilder.DropColumn(
+                name: "hasBath",
+                schema: "Identity",
+                table: "RoomTypes");
+
+            migrationBuilder.DropColumn(
+                name: "hasPhone",
+                schema: "Identity",
+                table: "RoomTypes");
+
+            migrationBuilder.DropColumn(
+                name: "hasTV",
+                schema: "Identity",
+                table: "RoomTypes");
+
+            migrationBuilder.DropColumn(
+                name: "hasWiFi",
+                schema: "Identity",
+                table: "RoomTypes");
+
+            migrationBuilder.RenameColumn(
+                name: "KidsCapacity",
+                schema: "Identity",
+                table: "RoomTypes",
+                newName: "Capacity");
+        }
+    }
+}
