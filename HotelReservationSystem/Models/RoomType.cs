28901948c133699ecfa99757d31209ac95c5975c
miljkovich <miljan6888@gmail.com>
2022-08-03 14:44:24 +0200

    Edited roomtype mdoel

--- HotelReservationSystem/Models/RoomType.cs
+++ HotelReservationSystem/Models/RoomType.cs
@@ -11,8 +11,10 @@ namespace HotelReservationSystem.Models
         [Display(Name = "Цена")]
         public int Price { get; set; }
 
-        [Display(Name = "Вместимость")]
-        public int Capacity { get; set; }
+        [Display(Name = "Вместимость Взрослых")]
+        public int AdultsCapacity { get; set; }
+        [Display(Name = "Вместимость Детей")]
+        public int KidsCapacity { get; set; }
 
         [Display(Name = "Описание")]
         public string ?Description { get; set; }
@@ -22,7 +24,14 @@ namespace HotelReservationSystem.Models
 
         [Display(Name = "Фото URL")]
         public string? RoomImageURL { get; set; }
-
+        [Display(Name = "Имеет ванную")]
+        public bool hasBath { get; set; }
+        [Display(Name = "Имеет телефон")]
+        public bool hasPhone { get; set; }
+        [Display(Name = "Имеет Wi-Fi")]
+        public bool hasWiFi { get; set; }
+        [Display(Name = "Имеет Телевизор")]
+        public bool hasTV { get; set; }
         public ICollection<Room>? Rooms { get; set; }
 
     }
