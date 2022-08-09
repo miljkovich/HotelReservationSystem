37a9aa525810ce763746307ad37342e10750efb6
miljkovich <miljan6888@gmail.com>
2022-08-09 14:13:54 +0200

    Added RoomTypeReservationViewModel

---
+++ HotelReservationSystem/Models/RoomTypeReservationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
+{
+    public class RoomTypeReservationViewModel
+    {
+        public int Id { get; set; }
+        public int RoomTypeId { get; set; }
+        public string RoomTypeName { get; set; } = null!;
+
+        public int AdultsCapacity { get; set; }
+        public int KidsCapacity { get; set; }
+        public string? RoomTypeDescription { get; set; }
+        public string? RoomTypeImageUrl { get; set; }
+        
+        [Required]
+        [Display(Name ="Взрослые")]
+        public int AdultsIn { get; set; }
+        [Required]
+        [Display(Name = "Дети")]
+        public int KidsIn { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+
+    }
+}
