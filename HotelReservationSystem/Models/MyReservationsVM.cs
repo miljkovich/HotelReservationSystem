b3b92007b502002c1c1daa5afbffe5d8de2ee040
miljkovich <miljan6888@gmail.com>
2022-08-23 13:37:23 +0200

    Added MyReservations View Model that is going to cointain information about Reservation and roomType and passed to MyReservationsView.

---
+++ HotelReservationSystem/Models/MyReservationsVM.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelReservationSystem.Models
+{
+    public class MyReservationsVM
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        public string RoomNumber { get; set; }
+        public int RoomTypeId { get; set; }
+        public string RoomTypeName { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+        public float Price { get; set; }
+    }
+}
